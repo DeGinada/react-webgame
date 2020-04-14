e076bf149dcd807c006a809e77c5be800f0dee98
DeGinada <deginada@gmail.com>
2020-04-14 16:38:13 +0900

    가위바위보2, 로또추첨기

--- client.jsx
+++ client.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
-import RSP from './RSP';
+import Lotto from './Lotto';
 
-const Hot = hot(RSP);
+const Hot = hot(Lotto);
 ReactDom.render(<Hot />, document.querySelector('#root'));
 
 
